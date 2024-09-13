5224961aa14a2c1b280dbdc5ac36975e293bc68a
Gerard van Helden <gerard@van-helden.name>
2024-09-13 21:26:24 +0200

    wip

---
+++ README.md
@@ -0,0 +1,8 @@
+# Encrypted files in git
+
+Basics: encrypted files are shared using a symmetric key. The symmetric key is
+encoded in the repo, under a file with a name based on a digest of the public
+ssh key.
+
+The symmetric key can thus only be obtained by someone who has the matching
+private key for any of the encryped symmetric key files.
---
+++ test-encrypt.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Public key encryption
+encrypt() {
+	local pubkey_file="$1"
+
+	openssl \
+		pkeyutl \
+		-encrypt \
+		-inkey <(ssh-keygen -f "$pubkey_file" -e -m pem 2>/dev/null) \
+		-pubin \
+		-out -
+}
+
+# RSA private key decryption
+decrypt() {
+	openssl \
+		pkeyutl \
+		-decrypt \
+		-inkey <(openssl rsa -in ~/.ssh/id_rsa -outform pem 2>/dev/null) \
+		-out -
+}
+
+digest() {
+	local pubkey_file="$1"
+	ssh-keygen -f $pubkey_file -e -m pem | openssl pkey -pubin -outform DER | openssl dgst -sha256 | awk '{print $2}'
+}
+
+_print_sym_key() {
+	cat ./keys/$(digest ~/.ssh/id_rsa.pub) | decrypt
+}
+
+share-key() {
+	local pubkey_file="$1"
+
+	_print_sym_key | encrypt $pubkey_file | ./keys/$(digest "$pubkey_file")
+}
+
+init-key() {
+	cat | encrypt ~/.ssh/id_rsa.pub > ./keys/$(digest ~/.ssh/id_rsa.pub)
+}
+
+symmetric-crypt() {
+	openssl enc -aes-256-cbc -pbkdf2 -iter 1000000 -salt -pass pass:$(_print_sym_key) $@ -out -
+}
+
+echo "Testing 123" | symmetric-crypt > encrypted
+cat ./encrypted | symmetric-crypt -d
+
