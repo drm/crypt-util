ad4a314b8907416550ad996023e9b3d8d80d45b6
Gerard van Helden <gerard@van-helden.name>
2024-09-15 01:02:50 +0200

    cleanup naming

--- crypt.sh
+++ crypt.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+HOME="$(cd ~ && pwd)"
+PRIVATE_KEY="${PRIVATE_KEY:-$HOME/.ssh/id_rsa}"
+PUBLIC_KEY="${PUBLIC_KEY:-$HOME/.ssh/id_rsa.pub}"
 KEYROOT="${KEYROOT:-./keys}"
+
 set -euo pipefail
 
 # Public key encryption
-encrypt() {
+_rsa-encrypt() {
 	local pubkey_file="$1"
 
 	openssl \
@@ -16,36 +20,47 @@ encrypt() {
 }
 
 # RSA private key decryption
-decrypt() {
+_rsa-decrypt() {
 	openssl \
 		pkeyutl \
 		-decrypt \
-		-inkey <(openssl rsa -in ~/.ssh/id_rsa -outform pem 2>/dev/null) \
+		-inkey <(openssl rsa -in $PRIVATE_KEY -outform pem 2>/dev/null) \
 		-out -
 }
 
-digest() {
+_digest() {
 	local pubkey_file="$1"
-	ssh-keygen -f $pubkey_file -e -m pem | openssl pkey -pubin -outform DER | openssl dgst -sha256 | awk '{print $2}'
+	ssh-keygen \
+		-f $pubkey_file \
+		-e \
+		-m pem \
+		| openssl pkey -pubin -outform DER \
+		| openssl dgst -sha256 \
+		| awk '{print $2}'
 }
 
-_print_sym_key() {
-	cat $KEYROOT/$(digest ~/.ssh/id_rsa.pub) | decrypt
+_sym_key() {
+	cat $KEYROOT/$(_digest "$PUBLIC_KEY") \
+		| _rsa-decrypt
 }
 
 share-key() {
 	local pubkey_file="$1"
-	_print_sym_key | encrypt $pubkey_file > $KEYROOT/$(digest "$pubkey_file")
+	_sym_key \
+		| _rsa-encrypt $pubkey_file \
+		> $KEYROOT/$(_digest "$pubkey_file")
 }
 
 init-key() {
-	cat | encrypt ~/.ssh/id_rsa.pub > $KEYROOT/$(digest ~/.ssh/id_rsa.pub)
+	cat \
+		| _rsa-encrypt $PUBLIC_KEY \
+		> $KEYROOT/$(_digest $PUBLIC_KEY)
 }
 
-symmetric-encrypt() { 
-	openssl enc -aes-256-cbc -pbkdf2 -salt -pass pass:$(_print_sym_key) $@ -out -
+encrypt() { 
+	openssl enc -aes-256-cbc -pbkdf2 -salt -pass pass:$(_sym_key) -out -
 }
 
-symmetric-decrypt() { 
-	openssl enc -d -aes-256-cbc -pbkdf2 -salt -pass pass:$(_print_sym_key) -out -
+decrypt() { 
+	openssl enc -d -aes-256-cbc -pbkdf2 -salt -pass pass:$(_sym_key) -out -
 }
